1db3eab0bca179915e01abef55d22cc15e9c7a34
Giuseppe Di Bona <giusedb@gmail.com>
2025-11-02 18:18:27 +0100

    Improve login

--- jsalchemy_authentication/manager.py
+++ jsalchemy_authentication/manager.py
@@ -2,9 +2,10 @@ from types import FunctionType
 
 import bcrypt
 from sqlalchemy import select
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, RelationshipProperty
 
 from jsalchemy_auth.sync.models import UserMixin
+from jsalchemy_auth.utils import invert_prop
 from .mixins import IdentityMixin
 from jsalchemy_web_context import ContextManager, db
 
@@ -26,7 +27,14 @@ class AuthenticationManager:
         self.field_set = {f.key for f in identity_model.__mapper__.attrs}
         self.unid = getattr(identity_model, identified_by)
         self.user_prop = user_prop
+        self.user_model = getattr(identity_model, user_prop).prop.entity.entity
+        self.inv_user_prop = self._inv_prop(getattr(self.identity_model, user_prop).prop).key
 
+    def _inv_prop(self, prop: RelationshipProperty):
+        if prop.back_populates:
+            return prop.entity.relationships[prop.back_populates]
+        if isinstance(prop.backref, str):
+            return prop.entity.relationships[prop.backref]
 
     async def login(self, username, password) -> DeclarativeBase | None:
         query = (select(self.identity_model)
@@ -43,9 +51,16 @@ class AuthenticationManager:
     async def register(self, user: dict) -> IdentityMixin:
         async with self.context():
             db_user = {k: v for k, v in user.items() if k in self.field_set}
-            user_obj = self.identity_model(**db_user)
+            user_mapper = self.user_model.__mapper__
+            identity_obj = self.identity_model(**db_user)
+            user_fields = {p.key for p in user_mapper.columns if p not in user_mapper.primary_key}
+            user_obj = self.user_model(**{k: v for k, v in user.items() if k in user_fields})
+            getattr(user_obj, self.inv_user_prop, identity_obj).append(identity_obj)
+            db.add(identity_obj)
             db.add(user_obj)
+            await db.flush()
             user['id'] = user_obj.id
+            await db.commit()
             return user
 
     async def user_exists(self, unid: str) -> bool:
